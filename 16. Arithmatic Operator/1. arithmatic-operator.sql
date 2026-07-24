d0df4d1d83dbe7168863c22a3ea78dab538a40f5
Niroj Shah <nirojshah1998@gmail.com>
2026-07-24 12:00:00 +0530

    Add SQL implementation for arithmetic operators

---
+++ 16. Arithmatic Operator/1. arithmatic-operator.sql
@@ -0,0 +1,15 @@
+-- Here we will use these operator to perform behind the numbers in the table.
+
+SELECT 10 + 2; -- 12
+
+SELECT 10 - 2; -- 8
+
+SELECT 10 / 2; -- 5
+
+SELECT 10 * 2; -- 20
+
+
+SELECT 5*4*3*2*1;
+
+
+SELECT 10 % 3;
